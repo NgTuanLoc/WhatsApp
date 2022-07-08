bfb8619f48b634dd01a017f7bb1ef3e56b05e58c
NgTuanLoc <18521011@gm.uit.edu.vn>
2022-07-08 12:49:42 +0700

    Fix Loading cmp bug

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -2,21 +2,23 @@ import { WhatsApp } from '@material-ui/icons';
 import styled from 'styled-components';
 import { ClipLoader } from 'react-spinners';
 
-const style = {
-  position: 'fixed',
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'center',
-  alignItems: 'center',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-};
+const style = {};
 
 const Loading = () => {
   return (
-    <div style={style}>
-      <WhatsApp style={{ 'font-size': '150px' }} />
+    <div
+      style={{
+        position: 'fixed',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+      }}
+    >
+      <WhatsApp style={{ fontSize: '150px' }} />
       <ClipLoader color="#3CBC28" size={80} />
     </div>
   );
